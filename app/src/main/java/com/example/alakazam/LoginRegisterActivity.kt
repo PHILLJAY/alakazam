39b7e4eb0556ec455f0faa20e0591bad4e99a622
Philip Jasionowski <47534916+PHILLJAY@users.noreply.github.com>
2023-11-04 22:09:28 -0400

    Update LoginRegisterActivity.kt
    Added comments to the loginregister activity

--- app/src/main/java/com/example/alakazam/LoginRegisterActivity.kt
+++ app/src/main/java/com/example/alakazam/LoginRegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.alakazam
 
+// Import necessary libraries and packages
 import android.content.Intent
 import android.os.Bundle
 import android.os.PersistableBundle
@@ -14,41 +15,50 @@ import com.google.firebase.firestore.FirebaseFirestore
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 
-
+// Define the LoginRegisterActivity class that inherits from AppCompatActivity
 class LoginRegisterActivity : AppCompatActivity() {
+    // Declare variables for the email field, password field, and login button
     private lateinit var emailField: EditText
     private lateinit var passwordField: EditText
     private lateinit var loginButton: Button
 
+    // Declare variables for Firebase authentication and Firestore database
     private lateinit var auth: FirebaseAuth
     private lateinit var db: FirebaseFirestore
 
+    // Override the onCreate function which is called when the activity is first created
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login_register)
 
+        // Log a debug message
         Log.d("Checkpoint", "lmao oncreate")
 
+        // Initialize Firebase authentication
         auth = Firebase.auth
 
+        // Sign out the current user
         auth.signOut()
 
+        // Initialize Firestore database
         db = Firebase.firestore
 
+        // Find the email field, password field, and login button in the layout
         emailField = findViewById(R.id.editTextTextEmailAddress)
         passwordField = findViewById(R.id.editTextTextPassword)
         loginButton = findViewById(R.id.loginButton)
 
-
-
+        // Set an onClick listener for the login button
         loginButton.setOnClickListener {
+            // Get the email and password entered by the user
             val email = emailField.text.toString()
             val password = passwordField.text.toString()
 
-            // Create new user , if it fails log them in instead lol
+            // Try to create a new user with the entered email and password
             auth.createUserWithEmailAndPassword(email, password)
                 .addOnCompleteListener { task ->
                     if (task.isSuccessful) {
+                        // If the user is created successfully, log a message and add the user to the database
                         Log.d("Auth", "CreateUserWithEmail:sucess")
 
                         val userData = hashMapOf(
@@ -65,11 +75,14 @@ class LoginRegisterActivity : AppCompatActivity() {
                             .addOnFailureListener { e ->
                                 Log.w("New User", "Error adding document", e)
                             }
+                        // Start the MainActivity
                         startActivity(Intent(this, MainActivity::class.java))
                     } else {
+                        // If the user creation fails, try to sign in the user with the entered email and password
                         auth.signInWithEmailAndPassword(email, password)
                             .addOnCompleteListener(this) { task ->
                                 if (task.isSuccessful) {
+                                    // If the sign in is successful, log a message and start the MainActivity
                                     Log.d("Auth", "signInWithEmail:success")
                                     //
                                     startActivity(Intent(this, MainActivity::class.java))
@@ -87,6 +100,3 @@ class LoginRegisterActivity : AppCompatActivity() {
         }
     }
 }
-
-
-
